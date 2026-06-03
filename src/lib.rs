ddc0492c4860a5010c73b27461db30d32e18ee72
Lars Schumann <LarsSchumann@mail.de>
2026-06-03 23:17:54 +0200

    remove unused features

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,18 @@
 #![feature(
-    macro_metavar_expr_concat,
-    min_generic_const_args,
+    const_cmp,
+    const_ops,
+    const_trait_impl,
+    derive_const,
+    f128,
+    f16,
     generic_const_args,
     generic_const_items,
     inherent_associated_types,
-    pattern_types,
-    pattern_type_macro,
-    const_ops,
-    const_trait_impl,
-    const_cmp,
     min_adt_const_params,
-    derive_const,
-    f16,
-    f128
+    min_generic_const_args
 )]
 #![allow(incomplete_features)]
 
 pub mod float;
 pub mod int;
 pub(crate) mod macros;
-
-#[macro_export]
-macro_rules! to_pattern_type {
-    (let $name:ident: $ty:ident is $lower:literal..=$upper:literal = $value:expr) => {
-        {
-            let __assert_type: $crate::int::${concat(R,$ty)}::<$lower, $upper> = $value;
-        }
-        let $name: pattern_type!(::core::primitive::$ty is $lower..=$upper) = unsafe { ::core::mem::transmute($value) };
-    };
-}
