feb2c24fc8d89f59b714764eab51a89a65eebfa7
Lars Schumann <LarsSchumann@mail.de>
2026-06-21 18:23:49 +0200

    feat: impl saturating_sub

--- exactly/src/macros/int.rs
+++ exactly/src/macros/int.rs
@@ -57,6 +57,10 @@ macro_rules! impl_int_common {
                 unsafe { $range_t_name::<{ ::tcm::$num_t::SATURATING_ADD::<LOWER, X> }, { ::tcm::$num_t::SATURATING_ADD::<UPPER, Y> }>::new_unchecked(self.inner().saturating_add(other.inner())) }
             }
 
+            pub const fn saturating_sub<const X: $num_t, const Y: $num_t>(self, other: $range_t_name<{ X }, { Y }>) -> $range_t_name<{ ::tcm::$num_t::SATURATING_SUB::<LOWER, Y> }, { ::tcm::$num_t::SATURATING_SUB::<UPPER, X> }> {
+                unsafe { $range_t_name::<{ ::tcm::$num_t::SATURATING_SUB::<LOWER, Y> }, { ::tcm::$num_t::SATURATING_SUB::<UPPER, X> }>::new_unchecked(self.inner().saturating_sub(other.inner())) }
+            }
+
         }
 
         impl<const A: $num_t, const B: $num_t, const X: $num_t, const Y: $num_t> const ::core::ops::Add<$range_t_name<{ X }, { Y }>> for $range_t_name<{ A }, { B }>{
