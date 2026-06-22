0eed11ad40d70d2d3e742ffb908c5ab1aa97a07e
Lars Schumann <LarsSchumann@mail.de>
2026-06-22 19:05:40 +0200

    feat: add normalize

--- funny/src/lib.rs
+++ funny/src/lib.rs
@@ -7,7 +7,8 @@
     unsized_const_params,
     const_closures,
     const_array,
-    const_convert
+    const_convert,
+    maybe_uninit_array_assume_init
 )]
 #![allow(incomplete_features)]
 
@@ -27,5 +28,15 @@ mod tests {
         let baz: Wu8<{ const { &[11, 21, 12, 22, 13, 23] } }> = foo.add(bar);
 
         let qux: Wu8<{ const { &[11, 12, 13, 21, 22, 23] } }> = baz.sort();
+    }
+
+    #[test]
+    fn tee() {
+        let foo: Wu8<{ const { &[1, 1, 1] } }> = Wu8(2);
+        let bar: Wu8<{ const { &[10, 20] } }> = Wu8(10);
+        let baz: Wu8<{ const { &[11, 21, 11, 21, 11, 21] } }> = foo.add(bar);
+
+        let _qux: Wu8<{ const { &[11, 11, 11, 21, 21, 21] } }> = baz.sort();
+        let _qox: Wu8<{ const { &[11, 21] } }> = baz.normalize();
     }
 }
--- funny/src/macros.rs
+++ funny/src/macros.rs
@@ -1,6 +1,8 @@
 macro_rules! impl_ints {
     ($([inner_type: $num_t:ident, wrap_t_name: $wrap_t_name:ident, extra_tcm: $extra_tcm:ident, sort_fn_name: $sort_fn_name:ident],)*) => {$(
 
+        #[derive(Debug, Copy, Clone,)]
+        #[repr(transparent)]
         pub struct $wrap_t_name<const VALUES: &'static [$num_t]>(pub $num_t);
 
         mod $extra_tcm {
@@ -13,9 +15,66 @@ macro_rules! impl_ints {
             pub(super) type const SORT<const SLICE: &'static[$num_t]>: &[$num_t] = const {
                 let arr: [$num_t; LEN::<SLICE>] = match SLICE.try_into() {
                     Ok(arr) => arr,
-                    Err(_) => panic!()
+                    Err(_) => unreachable!()
                 };
                 &::compile_time_sort::$sort_fn_name(arr)
+            };
+
+            pub(super) type const UNIQUE_COUNT<const SLICE: &'static[$num_t]>: usize = const {
+                let sorted = SORT::<SLICE>;
+                match sorted {
+                    [] => 0,
+                    [first, tail @ ..] => {
+                        let mut last_seen = *first;
+                        let mut unique_count: usize = 1;
+                        let mut i: usize = 0;
+
+                        while i < tail.len(){
+                            let current = tail[i];
+                            match current == last_seen {
+                                true => {},
+                                false => {
+                                    unique_count += 1;
+                                    last_seen = current;
+                                }
+                            }
+                            i += 1;
+                        }
+                        unique_count
+                    }
+                }
+            };
+
+            pub(super) type const NORMALIZE<const SLICE: &'static[$num_t]>: &[$num_t] = const {
+                use core::mem::MaybeUninit;
+                let sorted = SORT::<SLICE>;
+                match sorted {
+                    [] => &[],
+                    [first, tail @ ..] => {
+
+                        let mut mu: [MaybeUninit<$num_t>; UNIQUE_COUNT::<SLICE>] = [MaybeUninit::uninit(); UNIQUE_COUNT::<SLICE>];
+
+                        mu[0] = MaybeUninit::new(*first);
+
+                        let mut last_seen = *first;
+                        let mut unique_count: usize = 1;
+                        let mut i: usize = 0;
+
+                        while i < tail.len(){
+                            let current = tail[i];
+                            match current == last_seen {
+                                true => {},
+                                false => {
+                                    unique_count += 1;
+                                    mu[unique_count - 1] = MaybeUninit::new(current);
+                                    last_seen = current;
+                                }
+                            }
+                            i += 1;
+                        }
+                        &unsafe { MaybeUninit::array_assume_init(mu) }
+                    }
+                }
             };
         }
 
@@ -27,6 +86,10 @@ macro_rules! impl_ints {
 
             pub const fn sort(self) -> $wrap_t_name<{ $extra_tcm::SORT::<VALUES> }> {
                 $wrap_t_name::<{ $extra_tcm::SORT::<VALUES> }>(self.0)
+            }
+
+            pub const fn normalize(self) -> $wrap_t_name<{ $extra_tcm::NORMALIZE::<VALUES> }> {
+                $wrap_t_name::<{ $extra_tcm::NORMALIZE::<VALUES> }>(self.0)
             }
         }
 
