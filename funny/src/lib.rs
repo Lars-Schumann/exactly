059d5722e12a37a15a705de4a3377539902ea857
Lars Schumann <LarsSchumann@mail.de>
2026-06-23 00:30:43 +0200

    feat: change naming

--- funny/src/lib.rs
+++ funny/src/lib.rs
@@ -15,7 +15,7 @@
 
 mod macros;
 
-macros::impl_ints! {[inner_type: u8, wrap_t_name: Wu8, range_fn_name: range_u8, extra_tcm: extra_tcm_u8, sort_fn_name: into_sorted_u8_array],}
+macros::impl_ints! {[inner_type: u8, wrap_t_name: SetU8, range_fn_name: range_u8, extra_tcm: extra_tcm_u8, sort_fn_name: into_sorted_u8_array],}
 
 #[cfg(test)]
 mod tests {
@@ -23,56 +23,56 @@ mod tests {
 
     #[test]
     fn one() {
-        let foo: Wu8<{ const { &[1, 2, 3] } }> = Wu8::new(2).unwrap();
-        let bar: Wu8<{ const { &[10, 20] } }> = Wu8::new(10).unwrap();
+        let foo: SetU8<{ const { &[1, 2, 3] } }> = SetU8::new(2).unwrap();
+        let bar: SetU8<{ const { &[10, 20] } }> = SetU8::new(10).unwrap();
 
-        let baz: Wu8<{ const { &[11, 21, 12, 22, 13, 23] } }> = foo + bar;
+        let baz: SetU8<{ const { &[11, 21, 12, 22, 13, 23] } }> = foo + bar;
 
-        let _qux: Wu8<{ const { &[11, 12, 13, 21, 22, 23] } }> = baz.sort();
+        let _qux: SetU8<{ const { &[11, 12, 13, 21, 22, 23] } }> = baz.sort();
     }
 
     #[test]
     fn two() {
-        let foo: Wu8<{ const { &[1, 1, 1] } }> = Wu8::new(1).unwrap();
-        let bar: Wu8<{ const { &[10, 20] } }> = Wu8::new(10).unwrap();
-        let baz: Wu8<{ const { &[11, 21, 11, 21, 11, 21] } }> = foo + bar;
+        let foo: SetU8<{ const { &[1, 1, 1] } }> = SetU8::new(1).unwrap();
+        let bar: SetU8<{ const { &[10, 20] } }> = SetU8::new(10).unwrap();
+        let baz: SetU8<{ const { &[11, 21, 11, 21, 11, 21] } }> = foo + bar;
 
-        let _qux: Wu8<{ const { &[11, 11, 11, 21, 21, 21] } }> = baz.sort();
-        let _qox: Wu8<{ const { &[11, 21] } }> = baz.normalize();
+        let _qux: SetU8<{ const { &[11, 11, 11, 21, 21, 21] } }> = baz.sort();
+        let _qox: SetU8<{ const { &[11, 21] } }> = baz.normalize();
     }
 
     #[test]
     fn three() {
-        let foo: Wu8<{ const { &[1, 1, 1, 2, 2] } }> = Wu8::new(2).unwrap();
-        let bar: Wu8<{ const { &[1, 2, 3] } }> = Wu8::new(2).unwrap();
-        let baz: Wu8<{ const { &[2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5] } }> =
+        let foo: SetU8<{ const { &[1, 1, 1, 2, 2] } }> = SetU8::new(2).unwrap();
+        let bar: SetU8<{ const { &[1, 2, 3] } }> = SetU8::new(2).unwrap();
+        let baz: SetU8<{ const { &[2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5] } }> =
             (foo + bar).sort();
 
-        let _qox: Wu8<{ const { &[2, 3, 4, 5] } }> = baz.normalize();
+        let _qox: SetU8<{ const { &[2, 3, 4, 5] } }> = baz.normalize();
     }
 
     #[test]
     fn four() {
-        let foo: Wu8<{ const { &[2, 4] } }> = Wu8::new(2).unwrap();
-        let bar: Wu8<{ const { &[1, 2, 3] } }> = Wu8::new(3).unwrap();
-        let baz: Wu8<{ const { &[2, 4, 4, 6, 8, 12] } }> = (foo * bar).sort();
+        let foo: SetU8<{ const { &[2, 4] } }> = SetU8::new(2).unwrap();
+        let bar: SetU8<{ const { &[1, 2, 3] } }> = SetU8::new(3).unwrap();
+        let baz: SetU8<{ const { &[2, 4, 4, 6, 8, 12] } }> = (foo * bar).sort();
 
-        let _qox: Wu8<{ const { &[2, 4, 6, 8, 12] } }> = baz.normalize();
+        let _qox: SetU8<{ const { &[2, 4, 6, 8, 12] } }> = baz.normalize();
     }
 
     #[test]
     fn five() {
-        let four: Wu8<{ const { &[4] } }> = Wu8::new(4).unwrap();
-        let bar: Wu8<{ const { &[1, 2, 3] } }> = Wu8::new(3).unwrap();
+        let four: SetU8<{ const { &[4] } }> = SetU8::new(4).unwrap();
+        let bar: SetU8<{ const { &[1, 2, 3] } }> = SetU8::new(3).unwrap();
 
-        let _qox: Wu8<{ const { &[1, 2, 3] } }> = bar * four / four + four - four;
+        let _qox: SetU8<{ const { &[1, 2, 3] } }> = bar * four / four + four - four;
     }
 
     #[test]
     fn six() {
-        let r1: Wu8<{ extra_tcm_u8::RANGE::<0, 255> }> = Wu8::new(2).unwrap();
-        let r2: Wu8<{ extra_tcm_u8::RANGE::<1, 255> }> = Wu8::new(4).unwrap();
+        let r1: SetU8<{ extra_tcm_u8::RANGE::<0, 255> }> = SetU8::new(2).unwrap();
+        let r2: SetU8<{ extra_tcm_u8::RANGE::<1, 255> }> = SetU8::new(4).unwrap();
 
-        let p: Wu8<{ extra_tcm_u8::RANGE::<0, 255> }> = (r1 / r2).normalize();
+        let p: SetU8<{ extra_tcm_u8::RANGE::<0, 255> }> = (r1 / r2).normalize();
     }
 }
