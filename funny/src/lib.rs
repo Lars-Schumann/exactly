d1c2b032caf80c4a9581973ad0b65cc0b1374d70
Lars Schumann <LarsSchumann@mail.de>
2026-06-22 21:00:34 +0200

    tests: more tests

--- funny/src/lib.rs
+++ funny/src/lib.rs
@@ -22,17 +22,17 @@ mod tests {
     use super::*;
 
     #[test]
-    fn yee() {
+    fn one() {
         let foo: Wu8<{ const { &[1, 2, 3] } }> = Wu8::new(2).unwrap();
         let bar: Wu8<{ const { &[10, 20] } }> = Wu8::new(10).unwrap();
 
         let baz: Wu8<{ const { &[11, 21, 12, 22, 13, 23] } }> = foo + bar;
 
-        let qux: Wu8<{ const { &[11, 12, 13, 21, 22, 23] } }> = baz.sort();
+        let _qux: Wu8<{ const { &[11, 12, 13, 21, 22, 23] } }> = baz.sort();
     }
 
     #[test]
-    fn tee() {
+    fn two() {
         let foo: Wu8<{ const { &[1, 1, 1] } }> = Wu8::new(1).unwrap();
         let bar: Wu8<{ const { &[10, 20] } }> = Wu8::new(10).unwrap();
         let baz: Wu8<{ const { &[11, 21, 11, 21, 11, 21] } }> = foo + bar;
@@ -42,12 +42,29 @@ mod tests {
     }
 
     #[test]
-    fn gee() {
+    fn three() {
         let foo: Wu8<{ const { &[1, 1, 1, 2, 2] } }> = Wu8::new(2).unwrap();
         let bar: Wu8<{ const { &[1, 2, 3] } }> = Wu8::new(2).unwrap();
         let baz: Wu8<{ const { &[2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5] } }> =
             (foo + bar).sort();
 
         let _qox: Wu8<{ const { &[2, 3, 4, 5] } }> = baz.normalize();
+    }
+
+    #[test]
+    fn four() {
+        let foo: Wu8<{ const { &[2, 4] } }> = Wu8::new(2).unwrap();
+        let bar: Wu8<{ const { &[1, 2, 3] } }> = Wu8::new(3).unwrap();
+        let baz: Wu8<{ const { &[2, 4, 4, 6, 8, 12] } }> = (foo * bar).sort();
+
+        let _qox: Wu8<{ const { &[2, 4, 6, 8, 12] } }> = baz.normalize();
+    }
+
+    #[test]
+    fn five() {
+        let four: Wu8<{ const { &[4] } }> = Wu8::new(4).unwrap();
+        let bar: Wu8<{ const { &[1, 2, 3] } }> = Wu8::new(3).unwrap();
+
+        let _qox: Wu8<{ const { &[1, 2, 3] } }> = bar * four / four + four - four;
     }
 }
