f969316838a0734525c2f6592c8dc6bb275fc980
Lars Schumann <LarsSchumann@mail.de>
2026-06-23 21:38:46 +0200

    bench: w/e

--- funny/benches/compile_time.rs
+++ funny/benches/compile_time.rs
@@ -2,11 +2,37 @@
 
 #[cfg(feature = "bench_compile_time")]
 fn main() {
-    use funny::SetI32;
-    use funny::extra_tcm_i32;
+    {
+        use funny::SetU8;
+        use funny::extra_tcm_u8;
 
-    let r1: SetI32<{ extra_tcm_i32::RANGE::<0, 115> }> = SetI32::new(1).unwrap();
-    let r2: SetI32<{ extra_tcm_i32::RANGE::<1, 115> }> = SetI32::new(10).unwrap();
+        let a: SetU8<{ extra_tcm_u8::RANGE::<0, 127> }> = SetU8::new(1).unwrap();
+        let b: SetU8<{ extra_tcm_u8::RANGE::<0, 127> }> = SetU8::new(1).unwrap();
+        let c: SetU8<{ extra_tcm_u8::RANGE::<0, 254> }> = (a + b).normalize();
+        assert_eq!(c.inner(), 2);
 
-    let _q /* :SetI32<{ extra_tcm_i32::RANGE::<0, 115> }>*/ = (r1 / r2).normalize();
+        let x: SetU8<{ extra_tcm_u8::RANGE::<0, 255> }> = SetU8::new(1).unwrap();
+        let y: SetU8<{ extra_tcm_u8::RANGE::<1, 255> }> = SetU8::new(1).unwrap();
+        let z: SetU8<{ extra_tcm_u8::RANGE::<0, 255> }> = (x / y).normalize();
+        assert_eq!(z.inner(), 1);
+    }
+
+    {
+        use funny::SetI32;
+        use funny::extra_tcm_i32;
+
+        let a: SetI32<{ extra_tcm_i32::RANGE::<0, 50> }> = SetI32::new(1).unwrap();
+        let b: SetI32<{ extra_tcm_i32::RANGE::<0, 50> }> = SetI32::new(1).unwrap();
+        let c: SetI32<{ extra_tcm_i32::RANGE::<0, 100> }> = (a + b).normalize();
+        assert_eq!(c.inner(), 2);
+
+        let x: SetI32<{ extra_tcm_i32::RANGE::<0, 50> }> = SetI32::new(1).unwrap();
+        let y: SetI32<{ extra_tcm_i32::RANGE::<0, 50> }> = SetI32::new(1).unwrap();
+        let z = (x * y).normalize();
+        assert_eq!(z.inner(), 1);
+    }
+
+    println!("done!");
 }
+
+// 23.3s
