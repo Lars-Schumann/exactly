4cf7ab92ddc3893166d9f408487753cf765fcc9f
Lars Schumann <LarsSchumann@mail.de>
2026-06-27 21:17:21 +0200

    bench: make use of helper macros

--- funny/benches/compile_time.rs
+++ funny/benches/compile_time.rs
@@ -4,30 +4,30 @@
 fn main() {
     {
         use funny::SetU8;
-        use funny::extra_tcm_u8;
+        use funny::set_u8::Range;
 
-        let a: SetU8<{ extra_tcm_u8::RANGE::<0, 127> }> = SetU8::new(1).unwrap();
-        let b: SetU8<{ extra_tcm_u8::RANGE::<0, 127> }> = SetU8::new(1).unwrap();
-        let c: SetU8<{ extra_tcm_u8::RANGE::<0, 254> }> = (a + b).normalize();
+        let a: SetU8![Range![0..=127]] = SetU8::new(1).unwrap();
+        let b: SetU8![Range![0..=127]] = SetU8::new(1).unwrap();
+        let c: SetU8![Range![0..=254]] = (a + b).normalize();
         assert_eq!(c.inner(), 2);
 
-        let x: SetU8<{ extra_tcm_u8::RANGE::<0, 255> }> = SetU8::new(1).unwrap();
-        let y: SetU8<{ extra_tcm_u8::RANGE::<1, 255> }> = SetU8::new(1).unwrap();
-        let z: SetU8<{ extra_tcm_u8::RANGE::<0, 255> }> = (x / y).normalize();
+        let x: SetU8![Range![0..=255]] = SetU8::new(1).unwrap();
+        let y: SetU8![Range![1..=255]] = SetU8::new(1).unwrap();
+        let z: SetU8![Range![0..=255]] = (x / y).normalize();
         assert_eq!(z.inner(), 1);
     }
 
     {
         use funny::SetI32;
-        use funny::extra_tcm_i32;
+        use funny::set_i32::Range;
 
-        let a: SetI32<{ extra_tcm_i32::RANGE::<0, 50> }> = SetI32::new(1).unwrap();
-        let b: SetI32<{ extra_tcm_i32::RANGE::<0, 50> }> = SetI32::new(1).unwrap();
-        let c: SetI32<{ extra_tcm_i32::RANGE::<0, 100> }> = (a + b).normalize();
+        let a: SetI32![Range![0..=50]] = SetI32::new(1).unwrap();
+        let b: SetI32![Range![0..=50]] = SetI32::new(1).unwrap();
+        let c: SetI32![Range![0..=100]] = (a + b).normalize();
         assert_eq!(c.inner(), 2);
 
-        let x: SetI32<{ extra_tcm_i32::RANGE::<0, 50> }> = SetI32::new(1).unwrap();
-        let y: SetI32<{ extra_tcm_i32::RANGE::<0, 50> }> = SetI32::new(1).unwrap();
+        let x: SetI32![Range![0..=50]] = SetI32::new(1).unwrap();
+        let y: SetI32![Range![0..=50]] = SetI32::new(1).unwrap();
         let z = (x * y).normalize();
         assert_eq!(z.inner(), 1);
     }
@@ -42,3 +42,4 @@ fn main() {
 
 // 29.5s
 // 25.0s
+// 26.7s
