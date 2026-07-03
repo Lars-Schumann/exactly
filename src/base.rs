af1b7468e2c8aaee5bc45df49b6d40a3108fdd96
Lars Schumann <LarsSchumann@mail.de>
2026-07-03 19:04:23 +0200

    feat: port unnion and intersection

--- src/base.rs
+++ src/base.rs
@@ -38,6 +38,45 @@ pub const NORMALIZE<T: Copy + const Ord + ConstParamTy_ + Freeze + const Destruc
     normalized.const_make_global()
 }};
 
+pub const UNION<T: Freeze  + 'static + ConstParamTy_  + Copy, const SETS: &'static [&'static [T]]>: &[T] = const {
+        let mut onion: Vec<T> = Vec::new();
+        let mut i: usize = 0;
+
+        while i < SETS.len() {
+            let mut j: usize = 0;
+            while j < SETS[i].len() {
+                onion.push(SETS[i][j]);
+                j += 1;
+            }
+            i += 1;
+        }
+
+        onion.const_make_global()
+    };
+
+pub const INTERSECTION<T: Freeze  + 'static + ConstParamTy_  + Copy  + const PartialEq  + const Destruct, const SETS: &'static [&'static [T]]>: &[T] = const { 'out: {
+        let [first_set, ..] = SETS else {
+            break 'out &[];
+        };
+
+        let mut intersection: Vec<T> = Vec::with_capacity(first_set.len());
+
+        let mut i: usize = 0;
+        while i < first_set.len() {
+            intersection.push(first_set[i]);
+            i += 1;
+        }
+
+        let mut j: usize = 1;
+
+        while j < SETS.len() {
+            crate::const_helpers::ext_vec_reduce_to_intersection_with(&mut intersection, SETS[j]);
+            j += 1;
+        }
+
+        intersection.const_make_global()
+    }};
+
 pub(crate) const EMPTY<T: 'static>: &[T] = &[];
 
 pub(crate) const SLICEINATOR<T: 'static + ConstParamTy_  + Freeze, const NUM: T>: &[T] = const {
@@ -130,16 +169,16 @@ where
 
 base_macros::impl_ints! {
     the_dolla: $,
-    [inner_type: u8,    largest_num_t_with_same_signedness: u128,   wrap_t_name: SetU8,      private_macro_prefix: ඞඞ__private_macro_set_u8_,    extra_mod: set_u8    ],
-    [inner_type: u16,   largest_num_t_with_same_signedness: u128,   wrap_t_name: SetU16,     private_macro_prefix: ඞඞ__private_macro_set_u16_,   extra_mod: set_u16   ],
-    [inner_type: u32,   largest_num_t_with_same_signedness: u128,   wrap_t_name: SetU32,     private_macro_prefix: ඞඞ__private_macro_set_u32_,   extra_mod: set_u32   ],
-    [inner_type: u64,   largest_num_t_with_same_signedness: u128,   wrap_t_name: SetU64,     private_macro_prefix: ඞඞ__private_macro_set_u64_,   extra_mod: set_u64   ],
-    [inner_type: u128,  largest_num_t_with_same_signedness: u128,   wrap_t_name: SetU128,    private_macro_prefix: ඞඞ__private_macro_set_u128_,  extra_mod: set_u128  ],
-    [inner_type: usize, largest_num_t_with_same_signedness: usize,  wrap_t_name: SetUsize,   private_macro_prefix: ඞඞ__private_macro_set_usize_, extra_mod: set_usize ],
-    [inner_type: i8,    largest_num_t_with_same_signedness: i128,   wrap_t_name: SetI8,      private_macro_prefix: ඞඞ__private_macro_set_i8_,    extra_mod: set_i8    ],
-    [inner_type: i16,   largest_num_t_with_same_signedness: i128,   wrap_t_name: SetI16,     private_macro_prefix: ඞඞ__private_macro_set_i16_,   extra_mod: set_i16   ],
-    [inner_type: i32,   largest_num_t_with_same_signedness: i128,   wrap_t_name: SetI32,     private_macro_prefix: ඞඞ__private_macro_set_i32_,   extra_mod: set_i32   ],
-    [inner_type: i64,   largest_num_t_with_same_signedness: i128,   wrap_t_name: SetI64,     private_macro_prefix: ඞඞ__private_macro_set_i64_,   extra_mod: set_i64   ],
-    [inner_type: i128,  largest_num_t_with_same_signedness: i128,   wrap_t_name: SetI128,    private_macro_prefix: ඞඞ__private_macro_set_i128_,  extra_mod: set_i128  ],
-    [inner_type: isize, largest_num_t_with_same_signedness: isize,  wrap_t_name: SetIsize,   private_macro_prefix: ඞඞ__private_macro_set_isize_, extra_mod: set_isize ],
+    [inner_type: u8,    t_alias: SetU8,     largest_num_t_with_same_signedness: u128,   wrap_t_name: SetU8,      private_macro_prefix: ඞඞ__private_macro_set_u8_,    extra_mod: set_u8    ],
+    [inner_type: u16,   t_alias: SetU16,    largest_num_t_with_same_signedness: u128,   wrap_t_name: SetU16,     private_macro_prefix: ඞඞ__private_macro_set_u16_,   extra_mod: set_u16   ],
+    [inner_type: u32,   t_alias: SetU32,    largest_num_t_with_same_signedness: u128,   wrap_t_name: SetU32,     private_macro_prefix: ඞඞ__private_macro_set_u32_,   extra_mod: set_u32   ],
+    [inner_type: u64,   t_alias: SetU64,    largest_num_t_with_same_signedness: u128,   wrap_t_name: SetU64,     private_macro_prefix: ඞඞ__private_macro_set_u64_,   extra_mod: set_u64   ],
+    [inner_type: u128,  t_alias: SetU128,   largest_num_t_with_same_signedness: u128,   wrap_t_name: SetU128,    private_macro_prefix: ඞඞ__private_macro_set_u128_,  extra_mod: set_u128  ],
+    [inner_type: usize, t_alias: SetUsize,  largest_num_t_with_same_signedness: usize,  wrap_t_name: SetUsize,   private_macro_prefix: ඞඞ__private_macro_set_usize_, extra_mod: set_usize ],
+    [inner_type: i8,    t_alias: SetI8,     largest_num_t_with_same_signedness: i128,   wrap_t_name: SetI8,      private_macro_prefix: ඞඞ__private_macro_set_i8_,    extra_mod: set_i8    ],
+    [inner_type: i16,   t_alias: SetI16,    largest_num_t_with_same_signedness: i128,   wrap_t_name: SetI16,     private_macro_prefix: ඞඞ__private_macro_set_i16_,   extra_mod: set_i16   ],
+    [inner_type: i32,   t_alias: SetI32,    largest_num_t_with_same_signedness: i128,   wrap_t_name: SetI32,     private_macro_prefix: ඞඞ__private_macro_set_i32_,   extra_mod: set_i32   ],
+    [inner_type: i64,   t_alias: SetI64,    largest_num_t_with_same_signedness: i128,   wrap_t_name: SetI64,     private_macro_prefix: ඞඞ__private_macro_set_i64_,   extra_mod: set_i64   ],
+    [inner_type: i128,  t_alias: SetI128,   largest_num_t_with_same_signedness: i128,   wrap_t_name: SetI128,    private_macro_prefix: ඞඞ__private_macro_set_i128_,  extra_mod: set_i128  ],
+    [inner_type: isize, t_alias: SetIsize,  largest_num_t_with_same_signedness: isize,  wrap_t_name: SetIsize,   private_macro_prefix: ඞඞ__private_macro_set_isize_, extra_mod: set_isize ],
 }
