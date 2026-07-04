75d4c43306faca8c1a77e8cc7611468bbe6a8736
Lars Schumann <LarsSchumann@mail.de>
2026-07-04 17:55:55 +0200

    refactor: clean up impl macros unused param

--- src/macros.rs
+++ src/macros.rs
@@ -1,5 +1,5 @@
 macro_rules! impl_methods {
-    ($([num_t: $num_t:ident, extra_mod: $extra_mod:ident, cartesian_const_name: $cartesian_const_name:ident, fn_name: $fn_name:ident, fn_path: $fn_path:path]),+ $(,)?) => {$(
+    ($([num_t: $num_t:ident, cartesian_const_name: $cartesian_const_name:ident, fn_name: $fn_name:ident, fn_path: $fn_path:path]),+ $(,)?) => {$(
 
         mod ${concat(__,$num_t,_,$fn_name)} {
             use crate::base::Set;
@@ -33,7 +33,7 @@ macro_rules! impl_methods {
 pub(crate) use impl_methods;
 
 macro_rules! impl_ops {
-    ($([num_t: $num_t:ident, extra_mod: $extra_mod:ident, cartesian_const_name: $cartesian_const_name:ident, trait_fn_name: $trait_fn_name:ident, op_trait: $(::$op_trait:ident)+, op: $op:tt]),+ $(,)?) => {$(
+    ($([num_t: $num_t:ident, cartesian_const_name: $cartesian_const_name:ident, trait_fn_name: $trait_fn_name:ident, op_trait: $(::$op_trait:ident)+, op: $op:tt]),+ $(,)?) => {$(
 
         mod ${concat(__,$num_t,_,$trait_fn_name)} {
             use crate::base::Set;
@@ -147,31 +147,31 @@ macro_rules! impl_ints {
         }
 
         macros::impl_methods! {
-            [num_t: $num_t, extra_mod: $extra_mod, cartesian_const_name: CARTESIAN_STRICT_ADD    , fn_name: strict_add   , fn_path: ::core::primitive::$num_t::strict_add    ],
-            [num_t: $num_t, extra_mod: $extra_mod, cartesian_const_name: CARTESIAN_STRICT_SUB    , fn_name: strict_sub   , fn_path: ::core::primitive::$num_t::strict_sub    ],
-            [num_t: $num_t, extra_mod: $extra_mod, cartesian_const_name: CARTESIAN_STRICT_MUL    , fn_name: strict_mul   , fn_path: ::core::primitive::$num_t::strict_mul    ],
-            [num_t: $num_t, extra_mod: $extra_mod, cartesian_const_name: CARTESIAN_STRICT_DIV    , fn_name: strict_div   , fn_path: ::core::primitive::$num_t::strict_div    ],
+            [num_t: $num_t, cartesian_const_name: CARTESIAN_STRICT_ADD  , fn_name: strict_add   , fn_path: ::core::primitive::$num_t::strict_add    ],
+            [num_t: $num_t, cartesian_const_name: CARTESIAN_STRICT_SUB  , fn_name: strict_sub   , fn_path: ::core::primitive::$num_t::strict_sub    ],
+            [num_t: $num_t, cartesian_const_name: CARTESIAN_STRICT_MUL  , fn_name: strict_mul   , fn_path: ::core::primitive::$num_t::strict_mul    ],
+            [num_t: $num_t, cartesian_const_name: CARTESIAN_STRICT_DIV  , fn_name: strict_div   , fn_path: ::core::primitive::$num_t::strict_div    ],
 
-            [num_t: $num_t, extra_mod: $extra_mod, cartesian_const_name: CARTESIAN_WRAPPING_ADD  , fn_name: wrapping_add , fn_path: ::core::primitive::$num_t::wrapping_add  ],
-            [num_t: $num_t, extra_mod: $extra_mod, cartesian_const_name: CARTESIAN_WRAPPING_SUB  , fn_name: wrapping_sub , fn_path: ::core::primitive::$num_t::wrapping_sub  ],
-            [num_t: $num_t, extra_mod: $extra_mod, cartesian_const_name: CARTESIAN_WRAPPING_MUL  , fn_name: wrapping_mul , fn_path: ::core::primitive::$num_t::wrapping_mul  ],
-            [num_t: $num_t, extra_mod: $extra_mod, cartesian_const_name: CARTESIAN_WRAPPING_DIV  , fn_name: wrapping_div , fn_path: ::core::primitive::$num_t::wrapping_div  ],
+            [num_t: $num_t, cartesian_const_name: CARTESIAN_WRAPPING_ADD, fn_name: wrapping_add , fn_path: ::core::primitive::$num_t::wrapping_add  ],
+            [num_t: $num_t, cartesian_const_name: CARTESIAN_WRAPPING_SUB, fn_name: wrapping_sub , fn_path: ::core::primitive::$num_t::wrapping_sub  ],
+            [num_t: $num_t, cartesian_const_name: CARTESIAN_WRAPPING_MUL, fn_name: wrapping_mul , fn_path: ::core::primitive::$num_t::wrapping_mul  ],
+            [num_t: $num_t, cartesian_const_name: CARTESIAN_WRAPPING_DIV, fn_name: wrapping_div , fn_path: ::core::primitive::$num_t::wrapping_div  ],
         }
 
         macros::impl_ops! {
-            [num_t: $num_t, extra_mod: $extra_mod, cartesian_const_name: CARTESIAN_ADD      , trait_fn_name: add    , op_trait: ::core::ops::Add     , op: +  ],
-            [num_t: $num_t, extra_mod: $extra_mod, cartesian_const_name: CARTESIAN_SUB      , trait_fn_name: sub    , op_trait: ::core::ops::Sub     , op: -  ],
-            [num_t: $num_t, extra_mod: $extra_mod, cartesian_const_name: CARTESIAN_MUL      , trait_fn_name: mul    , op_trait: ::core::ops::Mul     , op: *  ],
-            [num_t: $num_t, extra_mod: $extra_mod, cartesian_const_name: CARTESIAN_DIV      , trait_fn_name: div    , op_trait: ::core::ops::Div     , op: /  ],
+            [num_t: $num_t, cartesian_const_name: CARTESIAN_ADD         , trait_fn_name: add    , op_trait: ::core::ops::Add     , op: +  ],
+            [num_t: $num_t, cartesian_const_name: CARTESIAN_SUB         , trait_fn_name: sub    , op_trait: ::core::ops::Sub     , op: -  ],
+            [num_t: $num_t, cartesian_const_name: CARTESIAN_MUL         , trait_fn_name: mul    , op_trait: ::core::ops::Mul     , op: *  ],
+            [num_t: $num_t, cartesian_const_name: CARTESIAN_DIV         , trait_fn_name: div    , op_trait: ::core::ops::Div     , op: /  ],
 
-            [num_t: $num_t, extra_mod: $extra_mod, cartesian_const_name: CARTESIAN_REM      , trait_fn_name: rem    , op_trait: ::core::ops::Rem     , op: %  ],
+            [num_t: $num_t, cartesian_const_name: CARTESIAN_REM         , trait_fn_name: rem    , op_trait: ::core::ops::Rem     , op: %  ],
 
-            [num_t: $num_t, extra_mod: $extra_mod, cartesian_const_name: CARTESIAN_BIT_AND  , trait_fn_name: bitand , op_trait: ::core::ops::BitAnd  , op: &  ],
-            [num_t: $num_t, extra_mod: $extra_mod, cartesian_const_name: CARTESIAN_BIT_OR   , trait_fn_name: bitor  , op_trait: ::core::ops::BitOr   , op: |  ],
-            [num_t: $num_t, extra_mod: $extra_mod, cartesian_const_name: CARTESIAN_BIT_XOR  , trait_fn_name: bitxor , op_trait: ::core::ops::BitXor  , op: ^  ],
+            [num_t: $num_t, cartesian_const_name: CARTESIAN_BIT_AND     , trait_fn_name: bitand , op_trait: ::core::ops::BitAnd  , op: &  ],
+            [num_t: $num_t, cartesian_const_name: CARTESIAN_BIT_OR      , trait_fn_name: bitor  , op_trait: ::core::ops::BitOr   , op: |  ],
+            [num_t: $num_t, cartesian_const_name: CARTESIAN_BIT_XOR     , trait_fn_name: bitxor , op_trait: ::core::ops::BitXor  , op: ^  ],
 
-            [num_t: $num_t, extra_mod: $extra_mod, cartesian_const_name: CARTESIAN_SHL      , trait_fn_name: shl    , op_trait: ::core::ops::Shl     , op: << ],
-            [num_t: $num_t, extra_mod: $extra_mod, cartesian_const_name: CARTESIAN_SHR      , trait_fn_name: shr    , op_trait: ::core::ops::Shr     , op: >> ],
+            [num_t: $num_t, cartesian_const_name: CARTESIAN_SHL         , trait_fn_name: shl    , op_trait: ::core::ops::Shl     , op: << ],
+            [num_t: $num_t, cartesian_const_name: CARTESIAN_SHR         , trait_fn_name: shr    , op_trait: ::core::ops::Shr     , op: >> ],
         }
 
     )*}
